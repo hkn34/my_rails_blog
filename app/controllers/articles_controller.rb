0ddc08c4d25e868a6bdf9b7ced3e5b0bf4299365
Houston Knight <houstonknight3434@gmail.com>
2019-11-27 18:04:30 -0600

    Add index action and add links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,10 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end

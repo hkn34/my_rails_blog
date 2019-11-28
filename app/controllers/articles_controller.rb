57d240185dd03bb5a7bc69cda6e7639c810172c2
Houston Knight <houstonknight3434@gmail.com>
2019-11-27 18:35:38 -0600

    Add update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
+  end
 
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -13,16 +17,22 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
   end
 
   private
